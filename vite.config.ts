0b511dbffc8caddabaebe89759779982a6a883a5
Vaibhav Pandey <vaibhavkpandeys@gmail.com>
2025-06-03 17:06:30 +0530

    changed port

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-      tailwindcss(),
-],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 3005,
+  }
 })
